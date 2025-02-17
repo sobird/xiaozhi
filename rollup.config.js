7ffdef52e144cd89cecd82f1d26c945f0a52ac1a
sobird <i@sobird.me>
2025-02-18 02:59:41 +0800

    feat: replace bin env tsx with  node

--- rollup.config.js
+++ rollup.config.js
@@ -7,40 +7,41 @@
  * @see https://cn.rollupjs.org/configuration-options
  * sobird<i@sobird.me> at 2023/09/28 11:30:37 created.
  */
-import { dirname, relative, extname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+// import { dirname, relative, extname } from 'node:path';
+// import { fileURLToPath } from 'node:url';
 
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
-import { glob } from 'glob';
+// import { glob } from 'glob';
 import { defineConfig } from 'rollup';
 import clear from 'rollup-plugin-clear';
 import copy from 'rollup-plugin-copy';
 import external from 'rollup-plugin-peer-deps-external';
 import typescript from 'rollup-plugin-typescript2';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 const isProduction = process.env.NODE_ENV === 'production';
 const DIST = isProduction ? 'dist' : 'dist';
 
-function input(pattern) {
-  return glob.sync(pattern, {
-    ignore: ['src/**/*.d.ts'],
-    cwd: __dirname,
-    absolute: false,
-  }).reduce((accu, filename) => {
-    accu[relative(
-      'src',
-      filename.slice(0, filename.length - extname(filename).length),
-    )] = filename;
-    return accu;
-  }, {});
-}
+// function input(pattern) {
+//   return glob.sync(pattern, {
+//     ignore: ['src/**/*.d.ts'],
+//     cwd: __dirname,
+//     absolute: false,
+//   }).reduce((accu, filename) => {
+//     accu[relative(
+//       'src',
+//       filename.slice(0, filename.length - extname(filename).length),
+//     )] = filename;
+//     return accu;
+//   }, {});
+// }
 
-const mainInput = input(['index.ts']);
+// const mainInput = input(['index.ts']);
 
 export default (env) => {
   return defineConfig([
@@ -72,6 +73,13 @@ export default (env) => {
           targets: [DIST],
           watch: false,
         }),
+        replace({
+          delimiters: ['', ''],
+          values: {
+            '#!/usr/bin/env tsx': '#!/usr/bin/env node',
+          },
+          preventAssignment: true,
+        }),
         external({
           includeDependencies: true,
         }),
@@ -90,9 +98,9 @@ export default (env) => {
         terser(),
         copy({
           targets: [
-            { src: 'package.json', dest: DIST },
-            { src: 'README.md', dest: DIST },
-            { src: 'LICENSE', dest: DIST },
+            // { src: 'package.json', dest: DIST },
+            // { src: 'README.md', dest: DIST },
+            // { src: 'LICENSE', dest: DIST },
           ],
           copyOnce: env.watch,
         }),
