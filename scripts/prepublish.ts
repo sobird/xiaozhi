83934d00d43fe0421ac1f1576e0e59cd7d9da007
sobird <i@sobird.me>
2025-02-18 10:22:27 +0800

    fix: update prepublish script

--- scripts/prepublish.ts
+++ scripts/prepublish.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 // scripts/prepublish.js
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import pkg from '../package.json' with { type: 'json' };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const packageJsonPath = path.join(__dirname, '../package.json');
 
